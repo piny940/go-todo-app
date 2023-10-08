82efc6f3e36bd0eaa801797e48e090c19399438b
mikan <83708535+piny940@users.noreply.github.com>
2023-10-08 22:38:40 +0900

    database.Initを作成

---
+++ database/init.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"database/sql"
+	"go-todo-app/config"
+)
+
+var db *sql.DB
+
+func Init() {
+	c := config.GetConfig()
+	var err error
+	db, err = sql.Open(c.GetString("db.provider"), c.GetString("db.url"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetDB() *sql.DB {
+	return db
+}
+
+func Close() {
+	db.Close()
+}
