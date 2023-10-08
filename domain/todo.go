137f19d29bcc99234f8515b308b5ecbcf64fc3c4
mikan <83708535+piny940@users.noreply.github.com>
2023-10-08 21:48:24 +0900

    domain/todoを作成

---
+++ domain/todo.go
@@ -0,0 +1,20 @@
+package domain
+
+import "time"
+
+type TodoID int
+type TodoTitle string
+type TodoStatus int
+
+const (
+	Active TodoStatus = iota
+	Completed
+)
+
+type Todo struct {
+	ID        TodoID     `json:"id"`
+	Title     TodoTitle  `json:"title"`
+	Status    TodoStatus `json:"status"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+}
