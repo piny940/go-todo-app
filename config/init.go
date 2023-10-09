c22fc44895111bff5b7f335b847ad5f5637c5823
mikan <83708535+piny940@users.noreply.github.com>
2023-10-09 21:33:19 +0900

    configのyamlの中に環境変数を埋め込めるようにする

--- config/init.go
+++ config/init.go
@@ -3,25 +3,44 @@ package config
 import (
 	"fmt"
 	"os"
+	"strings"
 
+	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 )
 
 var c *viper.Viper
 
 func Init(env string) {
+	godotenv.Load(fmt.Sprintf(".env.%s", env))
 	c = viper.New()
-	filename := fmt.Sprintf("config/environments/%s.yaml", env)
-	file, err := os.Open(filename)
+	yamlConfig := readYamlConfig(env)
+
+	configStr := os.ExpandEnv(yamlConfig)
+
+	c.SetConfigType("yaml")
+	err := c.ReadConfig(strings.NewReader(configStr))
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
-
-	c.SetConfigType("yaml")
-
 }
 
 func GetConfig() *viper.Viper {
 	return c
+}
+
+func readYamlConfig(env string) string {
+	filename := fmt.Sprintf("config/environments/%s.yaml", env)
+	configFile, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer configFile.Close()
+
+	fileContent := make([]byte, 1024)
+	_, err = configFile.Read(fileContent)
+	if err != nil {
+		panic(err)
+	}
+	return string(fileContent)
 }
