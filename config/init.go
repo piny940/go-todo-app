7674ddf48decd8c530fc4435c3262b37b4bcf43e
mikan <83708535+piny940@users.noreply.github.com>
2023-10-09 21:45:54 +0900

    DBに関する環境変数を.envから取得できるようにする

--- config/init.go
+++ config/init.go
@@ -2,8 +2,6 @@ package config
 
 import (
 	"fmt"
-	"os"
-	"strings"
 
 	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
@@ -14,33 +12,14 @@ var c *viper.Viper
 func Init(env string) {
 	godotenv.Load(fmt.Sprintf(".env.%s", env))
 	c = viper.New()
-	yamlConfig := readYamlConfig(env)
-
-	configStr := os.ExpandEnv(yamlConfig)
-
-	c.SetConfigType("yaml")
-	err := c.ReadConfig(strings.NewReader(configStr))
-	if err != nil {
+	c.SetConfigFile("yaml")
+	c.SetConfigName(env)
+	c.AddConfigPath("config/environments/")
+	if err := c.ReadInConfig(); err != nil {
 		panic(err)
 	}
 }
 
 func GetConfig() *viper.Viper {
 	return c
-}
-
-func readYamlConfig(env string) string {
-	filename := fmt.Sprintf("config/environments/%s.yaml", env)
-	configFile, err := os.Open(filename)
-	if err != nil {
-		panic(err)
-	}
-	defer configFile.Close()
-
-	fileContent := make([]byte, 1024)
-	_, err = configFile.Read(fileContent)
-	if err != nil {
-		panic(err)
-	}
-	return string(fileContent)
 }
