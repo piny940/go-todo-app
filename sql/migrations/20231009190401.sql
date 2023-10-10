2ab7a13e4271cb13935613bcb6e7ab1c77b7b226
mikan <83708535+piny940@users.noreply.github.com>
2023-10-10 11:39:57 +0900

    usersテーブルを作成

--- sql/migrations/20231009190401.sql
+++ sql/migrations/20231009190401.sql
@@ -1,7 +1,18 @@
-create table todos (
+create table if not exists users (
   id serial primary key,
-  title varchar not null,
-  status integer not null,
+  email varchar not null unique,
+  encrypted_password varchar not null,
   created_at timestamp not null default now(),
   updated_at timestamp not null default now()
 );
+
+create table if not exists todos (
+  id serial primary key,
+  title varchar not null,
+  status integer not null,
+  user_id integer not null references users(id),
+  created_at timestamp not null default now(),
+  updated_at timestamp not null default now()
+);
+
+create index if not exists todos_user_id_index on todos(user_id);
