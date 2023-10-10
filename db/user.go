c63f47940c65733d14d384fd475e7c6dd07f0c79
mikan <83708535+piny940@users.noreply.github.com>
2023-10-10 11:21:01 +0900

    dbのuser Listを作成

--- db/user.go
+++ db/user.go
@@ -10,6 +10,31 @@ func NewUserRepo(db *DB) *userRepo {
 	return &userRepo{db: db}
 }
 
+func (u *userRepo) List() ([]*domain.User, error) {
+	var users = make([]*domain.User, 0)
+	rows, err := u.db.Client.Query("select * from users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user domain.User
+		if err := rows.Scan(
+			&user.ID,
+			&user.Email,
+			&user.EncryptedPassword,
+			&user.CreatedAt,
+			&user.UpdatedAt,
+		); err != nil {
+			return nil, err
+		}
+		users = append(users, &user)
+	}
+
+	return users, nil
+}
+
 func (u *userRepo) Create(email domain.UserEmail, password domain.UserEncryptedPassword) (*domain.User, error) {
 	var user domain.User
 	if err := u.db.Client.QueryRow(
--- domain/repository/user_repo.go
+++ domain/repository/user_repo.go
@@ -3,5 +3,6 @@ package repository
 import "go-todo-app/domain"
 
 type IUserRepo interface {
+	List() ([]*domain.User, error)
 	Create(email domain.UserEmail, password domain.UserEncryptedPassword) (*domain.User, error)
 }
