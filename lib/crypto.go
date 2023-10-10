f0b54cd97aed77c9938974e599cacf5d96c2605d
mikan <83708535+piny940@users.noreply.github.com>
2023-10-10 10:04:15 +0900

    cryptoを作成

---
+++ lib/crypto.go
@@ -0,0 +1,13 @@
+package lib
+
+import "golang.org/x/crypto/bcrypt"
+
+func EncryptPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hash), err
+}
+
+func ComparePassword(hashedPassword, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
+}
