2c32efda163583320acc8e00a06e7373d00c1615
mikan <83708535+piny940@users.noreply.github.com>
2023-10-10 10:08:15 +0900

    NewUserを作成

--- domain/user.go
+++ domain/user.go
@@ -1,15 +1,30 @@
 package domain
 
-import "time"
+import (
+	"go-todo-app/lib"
+	"time"
+)
 
 type UserID uint
 type UserEmail string
-type UserPassword string
+type UserRawPassword string
+type UserEncryptedPassword string
 
 type User struct {
-	ID        UserID       `json:"id"`
-	Email     UserEmail    `json:"email"`
-	Password  UserPassword `json:"password"`
-	CreatedAt time.Time    `json:"created_at"`
-	UpdatedAt time.Time    `json:"updated_at"`
+	ID                UserID                `json:"id"`
+	Email             UserEmail             `json:"email"`
+	EncryptedPassword UserEncryptedPassword `json:"password"`
+	CreatedAt         time.Time             `json:"created_at"`
+	UpdatedAt         time.Time             `json:"updated_at"`
+}
+
+func NewUser(email UserEmail, rawPassword UserRawPassword) (*User, error) {
+	encryptedPassword, err := lib.EncryptPassword(string(rawPassword))
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		Email:             email,
+		EncryptedPassword: UserEncryptedPassword(encryptedPassword),
+	}, nil
 }
